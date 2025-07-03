6d5ddd748032a0c4b206aa1ba393b491779c84e1
prakharsinghch <prakharsinghchouhan21@gmail.com>
2025-07-03 11:49:17 +0530

    Property-sorted

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -7,7 +7,7 @@ router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant)
 router.post("/", createTenant);
 router.get("/:cognitoId/current-residences", getCurrentResidences);
-router.post("/:cognitoId/favourites/:propertyId",addFavouriteProperty );
-router.delete("/:cognitoId/favourites/:propertyId",removeFavouriteProperty);
+router.post("/:cognitoId/favorites/:propertyId",addFavouriteProperty );
+router.delete("/:cognitoId/favorites/:propertyId",removeFavouriteProperty);
 
 export default router;
